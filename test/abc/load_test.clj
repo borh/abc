65cd0f533116ae3c3b2cce797922389457771ec4
Bor Hodošček <dev@bor.space>
2026-07-04 14:01:37 +0900

    test: remove XTDB require and persist-texts test from abc.load-test
    The persist-texts-uses-supplied-node-for-reads-test asserted the old
    [:xtdb.api/put ...] tx shape; with XTDB retired there is no tx path to
    assert. Retains the load and (skipped) extract-texts tests.

--- test/abc/load_test.clj
+++ test/abc/load_test.clj
@@ -3,8 +3,7 @@
             [abc.test-utils :refer :all]
             [abc.tools.malli :as am]
             [abc.load :as load :refer :all]
-            [abc.config :as config]
-            [abc.xtdb :as xtdb]))
+            [abc.config :as config]))
 
 (def ^:dynamic ^:private *db* nil)
 
@@ -25,34 +24,8 @@
     ;; A full database validation is slow
     (is (schema-valid :abc.aozora/db-entries *db*))))
 
-(deftest persist-texts-uses-supplied-node-for-reads-test
-  (testing "persist-texts! keeps read and write paths on the explicit node"
-    (let [submitted (atom nil)]
-      (reset! load/!times [])
-      (with-redefs [xtdb/node (fn []
-                                (throw (ex-info "default XTDB node should not be used" {})))
-                    xtdb/all-works (fn [& {:keys [node]}]
-                                     (is (= ::explicit-node node))
-                                     #{:abc.aozora/w043661})
-                    load/work-id->document (fn [node id]
-                                             (is (= ::explicit-node node))
-                                             (is (= :abc.aozora/w043661 id))
-                                             {:document/metadata {:abc.aozora/work-id id}})
-                    xtdb/submit-tx (fn [node tx]
-                                     (reset! submitted {:node node :tx (doall tx)}))]
-        (load/persist-texts! ::explicit-node)
-        (is (= ::explicit-node (:node @submitted)))
-        (is (= [[:xtdb.api/put {:document/metadata {:abc.aozora/work-id :abc.aozora/w043661}
-                                 :xt/id :abc.aozora/d043661}]]
-               (:tx @submitted)))))))
-
 ;; Skipped: Aozora text parsing moves out of Clojure; consumed as JSON AST
 ;; from an external parser per the parser-IR contract.
 (deftest ^:kaocha/skip extract-texts-test
   (testing "Extracting texts from database fixture"
-    ;; Good testcase:
-    ;; :abc.aozora/w043688
-    #_(is (schema-validate :document/body :TODO))
-    #_(doseq [doc (take 10 (extract-texts *db*))]
-        (is (schema-valid :document/body doc)
-            #_(nil? (dorun (extract-texts *db*)))))))
+    #_(is (schema-validate :document/body :TODO))))
